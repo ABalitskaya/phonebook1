b88d9df180199e17aed051aa34d34046a83fa8e1
Alexandra Balitskaya <a.balickaya@gmail.com>
2023-02-01 21:02:20 +0400

    Added test EditContactInfo

---
+++ src/test/java/EditContactInfo.java
@@ -0,0 +1,35 @@
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+public class EditContactInfo extends Login {
+
+    By searchField = By.id("input-search-contact");
+    By myCreatedContact = By.cssSelector("div[class='d-flex justify-content-between'] b");
+    By editButton = By.id("btn-edit-contact");
+
+    By descriptionField = By.name("input-ec-description");
+
+    By buttonSave = By.cssSelector("button[class='btn btn-primary submit-btn-ec']");
+
+
+    @Test
+    public void editContactInformation() {
+        String contactFirstName = "Alexandra";
+        String contactLastName = "Balitskaya";
+        String firstAndLastName = contactFirstName + contactLastName;
+        driver.findElement(searchField).sendKeys(firstAndLastName);
+        driver.findElement(myCreatedContact).click();
+        driver.findElement(editButton).click();
+        driver.findElement(descriptionField).click();
+        String firstPartOfExpectedDescription = driver.findElement(descriptionField).getText();
+
+        String newDescription = "Updated description for test";
+        fillField(newDescription, descriptionField);
+        driver.findElement(buttonSave).click();
+
+        String actualTextOfDescription = driver.findElement(descriptionField).getText();
+        String expectedTextOfDescription = firstPartOfExpectedDescription + newDescription;
+        //Assert.assertEquals(expectedTextOfDescription, actualTextOfDescription);
+
+    }
+}
